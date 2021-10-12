d2103626666f0aadfbdd27f0132dbb369ce2253f
NayLin1590 <naylintbk15@gmail.com>
2021-10-12 09:57:51 +0630

    create vue frame

---
+++ src/store/index.js
@@ -0,0 +1,53 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import createPersistedState from "vuex-persistedstate";
+
+Vue.use(Vuex);
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+
+export default new Vuex.Store({
+    state: {
+        user: null,
+    },
+    mutations: {
+        setUserData(state, userData) {
+            state.user = userData;
+        },
+    },
+    actions: {
+        login({ commit }, credentials) {
+            return axios.post("/auth/login", credentials).then(({ data }) => {
+                console.log(data)
+                commit("setUserData", data);
+            });
+        },
+        logout({ commit }) {
+            // return axios.post("/auth/logout", credentials).then(() => {
+            //     commit("setUserData", null);
+            // });
+            commit("setUserData", null)
+        },
+    },
+    getters: {
+        isLoggedIn: (state) => !!state.user,
+        userType: (state) => {
+            if (state.user && state.user.user.user_type) {
+                return state.user.data.user_type;
+            }
+            return -1;
+        },
+        userId: (state) => {
+            if (state.user && state.user.data.user_id) {
+                return state.user.data.user_id;
+            }
+        },
+        userName: (state) => {
+            if (state.user && state.user.user.name) {
+                return state.user.user.name;
+            }
+        },
+    },
+    plugins: [createPersistedState()],
+});
